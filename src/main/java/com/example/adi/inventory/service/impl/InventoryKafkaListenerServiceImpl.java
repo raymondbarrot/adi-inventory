993e7b4dfb5b02a42ec23ebf3091da55b4f8f3e2
Raymond Barrot <raymond.roman@collabera.com>
2025-03-29 20:44:51 +0800

    added logging

--- src/main/java/com/example/adi/inventory/service/impl/InventoryKafkaListenerServiceImpl.java
+++ src/main/java/com/example/adi/inventory/service/impl/InventoryKafkaListenerServiceImpl.java
@@ -2,14 +2,16 @@ package com.example.adi.inventory.service.impl;
 
 import com.example.adi.inventory.model.BidOffer;
 import com.example.adi.inventory.service.InventoryKafkaListenerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class InventoryKafkaListenerServiceImpl implements InventoryKafkaListenerService {
     @Override
     @KafkaListener(topics = "${kafka.topic.recommendation}", groupId = "recommendation-group")
     public void consumeMessage(BidOffer bidOffer) {
-
+        log.info("Consumed: {}", bidOffer);
     }
 }
--- src/main/java/com/example/adi/inventory/service/impl/InventoryKafkaProducerServiceImpl.java
+++ src/main/java/com/example/adi/inventory/service/impl/InventoryKafkaProducerServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.adi.inventory.service.impl;
 import com.example.adi.inventory.config.ConstantsConfig;
 import com.example.adi.inventory.model.InventoryRequest;
 import com.example.adi.inventory.service.InventoryKafkaProducerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class InventoryKafkaProducerServiceImpl implements InventoryKafkaProducerService {
     @Autowired
@@ -15,6 +17,7 @@ public class InventoryKafkaProducerServiceImpl implements InventoryKafkaProducer
     private KafkaTemplate<String, InventoryRequest> kafkaTemplate;
     @Override
     public void publish(InventoryRequest inventoryRequest) {
+        log.info("Produced: {}", inventoryRequest);
         this.kafkaTemplate.send(constantsConfig.getRecommendationTopic(), inventoryRequest);
     }
 }
